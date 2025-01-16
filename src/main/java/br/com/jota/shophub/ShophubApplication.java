d82cc8c0ca44d3f1b3e8dac2d6238167ac24031d
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2025-01-15 21:00:14 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/com/jota/shophub/ShophubApplication.java
@@ -0,0 +1,13 @@
+package br.com.jota.shophub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShophubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShophubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jota/shophub/ShophubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jota.shophub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShophubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
